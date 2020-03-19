148fbee0eae14442ac9b2548e2abcc44ea7a6319
SarumanW <olgasergeevna0803@gmail.com>
2020-03-19 18:42:51 +0300

    Fix collective.

--- src/main/java/edu/coursera/distributed/CollectiveMatrixMult.java
+++ src/main/java/edu/coursera/distributed/CollectiveMatrixMult.java
@@ -28,22 +28,12 @@ public class CollectiveMatrixMult {
             endRow = nRows;
         }
 
-        mpi.MPI_Bcast(a.getValues(), 0, a.getNRows() * a.getNCols(),
-                0, mpi.MPI_COMM_WORLD);
-        mpi.MPI_Bcast(b.getValues(), 0, b.getNRows() * b.getNCols(),
-                0, mpi.MPI_COMM_WORLD);
-
-        for (int i = 0; i < c.getNRows(); i++) {
-            for (int j = 0; j < c.getNCols(); j++) {
-                c.set(i, j, 0.0);
-
-                for (int k = 0; k < b.getNRows(); k++) {
-                    c.incr(i, j, a.get(i, k) * b.get(k, j));
-                }
-            }
-        }
-
         if (myRank == 0) {
+            mpi.MPI_Bcast(a.getValues(), 0, a.getNRows() * a.getNCols(),
+                    0, mpi.MPI_COMM_WORLD);
+            mpi.MPI_Bcast(b.getValues(), 0, b.getNRows() * b.getNCols(),
+                    0, mpi.MPI_COMM_WORLD);
+
 
             MPI.MPI_Request[] requests = new MPI.MPI_Request[size - 1];
 
@@ -64,6 +54,17 @@ public class CollectiveMatrixMult {
 
             mpi.MPI_Waitall(requests);
         } else {
+
+            for (int i = startRow; i < endRow; i++) {
+                for (int j = 0; j < c.getNCols(); j++) {
+                    c.set(i, j, 0.0);
+
+                    for (int k = 0; k < b.getNRows(); k++) {
+                        c.incr(i, j, a.get(i, k) * b.get(k, j));
+                    }
+                }
+            }
+
             mpi.MPI_Send(c.getValues(), startRow * c.getNCols(),
                     (endRow - startRow) * c.getNCols(), 0, myRank,
                     mpi.MPI_COMM_WORLD);
